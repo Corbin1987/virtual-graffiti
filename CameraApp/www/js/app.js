c5a87f2fc98e59e8e5a49f7c646493cd534a5cf8
Corbin Via <email": null>
2016-11-28 14:57:37 -0600

    Add google maps api key to .gitignore file

--- CameraApp/www/js/app.js
+++ CameraApp/www/js/app.js
@@ -66,40 +66,26 @@ app.controller('CameraCtrl', function($scope, $cordovaCamera) {
 });
 
 app.controller('MapController', function($scope, $cordovaGeolocation, $ionicLoading, $ionicPlatform) {
-    
-   $ionicPlatform.ready(function() {
-        
-       $ionicLoading.show({
-           template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
-       });
-        
-       var posOptions = {
-           enableHighAccuracy: true,
-           timeout: 20000,
-           maximumAge: 0
-       };
-       $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
-           var lat  = position.coords.latitude;
-           var long = position.coords.longitude;
-            
-           var myLatlng = new google.maps.LatLng(lat, long);
-            
-           var mapOptions = {
-               center: myLatlng,
-               zoom: 16,
-               mapTypeId: google.maps.MapTypeId.ROADMAP
-           };          
-            
-           var map = new google.maps.Map(document.getElementById("map"), mapOptions);          
-            
-           $scope.map = map;   
-           $ionicLoading.hide();           
-            
-       }, function(err) {
-           $ionicLoading.hide();
-           console.log(err);
-       });
-   });               
+  $ionicPlatform.ready(function() {
+    $ionicLoading.show({
+      template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+    });
+
+  var posOptions = { enableHighAccuracy: true, timeout: 20000, maximumAge: 0 };
+
+  $cordovaGeolocation.getCurrentPosition(posOptions).then(function(position) {
+    var lat  = position.coords.latitude;
+    var lng = position.coords.longitude;
+    var myLatlng = new google.maps.LatLng(lat, lng);
+    var mapOptions = { center: myLatlng, zoom: 16, mapTypeId: google.maps.MapTypeId.ROADMAP };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    $scope.map = map;
+    $ionicLoading.hide();},
+    function(err) {
+      $ionicLoading.hide();
+      console.log(err);
+    });
+  });
 });
 
 
