fbe4ae2576c6272d9bcf7db306b967752d126002
busster <jasonmbuss@gmail.com>
2016-11-29 17:36:32 -0600

    progress

--- CameraApp/www/js/app.js
+++ CameraApp/www/js/app.js
@@ -60,14 +60,14 @@ app.controller('CameraCtrl', function($scope, $cordovaCamera, $cordovaGeolocatio
     $cordovaCamera.getPicture(options).then(function(data) {
         // console.log("camera data " + angular.toJson(data));
         pictureData = 'data:image/jpeg;base64,' + data;
-        console.log(pictureData)
+        // console.log(pictureData)
         $scope.show = true;
         
         $scope.pictureUrl = pictureData;
 
         getCoords().then(function(position) {
           coordData = {lat: position.coords.latitude, long: position.coords.longitude};
-          console.log(coordData)
+          // console.log(coordData)
           $scope.disabled = false;
           //enable the button
         }), function(error) {
@@ -83,7 +83,15 @@ app.controller('CameraCtrl', function($scope, $cordovaCamera, $cordovaGeolocatio
     return $cordovaGeolocation.getCurrentPosition(posOptions)
   }
 
+  function canvasSave(){
+    return canvasData = canvas.toDataUrl();
+  }
+
   $scope.savePicture = function(){
+    canvasData = canvas.toDataURL();
+
+    // canvasSave();
+
 
     // ADD A SAVING TAG
 
@@ -92,9 +100,9 @@ app.controller('CameraCtrl', function($scope, $cordovaCamera, $cordovaGeolocatio
     $http({
     method: 'POST',
     url : 'http://172.16.0.12:3000/pictures',
-    data: {params: {pictureData: pictureData,coordData: coordData}}
+    data: {params: {pictureData: pictureData, coordData: coordData, canvasData: canvasData}}
     }).then(function successCallback(response) {
-      console.log("in successCallback")
+      // console.log("in successCallback")
       // $scope.class = "bot";
       $scope.show = false;
       $scope.disabled = true;
@@ -105,10 +113,124 @@ app.controller('CameraCtrl', function($scope, $cordovaCamera, $cordovaGeolocatio
     }, function errorCallback(response) {
       console.log(response);
     });
-
-
-
   }
+
+/////////////////////////////////////////////////////// DRAWING JS //////////////////////////////////////////////////////////////////
+
+  var canvasData;
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var drawingColor = "#000000";
+
+    // Set up mouse events for drawing
+  var drawing = false;
+  var mousePos = { x:0, y:0 };
+  var lastPos = mousePos;
+  canvas.addEventListener("mousedown", function (e) {
+          drawing = true;
+    lastPos = getMousePos(canvas, e);
+  }, false);
+  canvas.addEventListener("mouseup", function (e) {
+    drawing = false;
+  }, false);
+  canvas.addEventListener("mousemove", function (e) {
+    mousePos = getMousePos(canvas, e);
+  }, false);
+
+  // Get the position of the mouse relative to the canvas
+  function getMousePos(canvasDom, mouseEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+      x: mouseEvent.clientX - rect.left,
+      y: mouseEvent.clientY - rect.top
+    };
+  }
+  window.requestAnimFrame = (function (callback) {
+          return window.requestAnimationFrame ||
+             window.webkitRequestAnimationFrame ||
+             window.mozRequestAnimationFrame ||
+             window.oRequestAnimationFrame ||
+             window.msRequestAnimaitonFrame ||
+             function (callback) {
+          window.setTimeout(callback, 1000/60);
+             };
+  })();
+
+  function renderCanvas() {
+    if (drawing) {
+      ctx.moveTo(lastPos.x, lastPos.y);
+      ctx.lineTo(mousePos.x, mousePos.y);
+      ctx.stroke();
+      lastPos = mousePos;
+    }
+  }
+
+  // Allow for animation
+  (function drawLoop () {
+    requestAnimFrame(drawLoop);
+    renderCanvas();
+  })();
+
+  canvas.addEventListener("touchstart", function (e) {
+          mousePos = getTouchPos(canvas, e);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousedown", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
+  canvas.addEventListener("touchend", function (e) {
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
+  canvas.addEventListener("touchmove", function (e) {
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousemove", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
+
+  // Get the position of a touch relative to the canvas
+  function getTouchPos(canvasDom, touchEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+      x: touchEvent.touches[0].clientX - rect.left,
+      y: touchEvent.touches[0].clientY - rect.top
+    };
+  }
+
+  // Prevent scrolling when touching the canvas
+  document.body.addEventListener("touchstart", function (e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+  document.body.addEventListener("touchend", function (e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+  document.body.addEventListener("touchmove", function (e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////// DRAWING JS //////////////////////////////////////////////////////////////////
+
 });
 
 
--- Server/db/migrate/20161127232020_create_pictures.rb
+++ Server/db/migrate/20161127232020_create_pictures.rb
@@ -2,6 +2,7 @@ class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
       t.attachment :image, { null: false }
+      t.attachment :drawn_image, { null: false }
       t.integer :location_id, { null: false }
 
       t.timestamps(null: false)
